5396ad2c8667442b1ff30e57f61ffac86c1a18d8
aliaksah <aliaksah@math.uio.no>
2017-10-02 12:51:12 +0200

    epi DGLMM example edited

--- examples/Epigenetic Data/par_epi.r
+++ examples/Epigenetic Data/par_epi.r
@@ -3,70 +3,9 @@
 # cat slurm-16078690.out
 # squeue -u aliaksah
 #
-
+library("RCurl")
 source("https://raw.githubusercontent.com/aliaksah/EMJMCMC2016/master/R/the_mode_jumping_package4.r")
 
-
-library(inline)
-includes <- '#include <sys/wait.h>'
-code <- 'int wstat; while (waitpid(-1, &wstat, WNOHANG) > 0) {};'
-wait <- cfunction(body=code, includes=includes, convention='.C')
-
-
-estimate.inla.poisson <- function(formula, data)
-{
-
-  out<-NULL
-  capture.output({tryCatch(capture.output({
-    out <-inla(family = "poisson",data = data,formula = formula,control.compute = list(dic = TRUE, waic = TRUE, mlik = TRUE))
-  }))})
-  if(is.null(out))
-    return(list(mlik = -10000,waic =  10000 , dic = 10000, summary.fixed =list(mean = NULL)))
-  # use dic and aic as bic and aic correspondinly
-  coef<-out$summary.fixed$mode
-  #coef[1]<-coef[1]+out$summary.hyperpar$mode[1]
-  return(list(mlik = out$mlik[1],waic =  out$waic[1] , dic = out$dic[1], summary.fixed =list(mean = coef)))
-
-}
-
-
-parall.gmj <<- mclapply
-
-
-
-simplifyposteriors<-function(X,posteriors,th=0.0001,thf=0.25)
-{
-  posteriors<-posteriors[-which(posteriors[,2]<th),]
-  rhash<-hash()
-  for(i in 1:length(posteriors[,1]))
-  {
-    expr<-posteriors[i,1]
-    print(expr)
-    res<-model.matrix(data=X,object = as.formula(paste0("methylated_bases~",expr)))
-    ress<-c(stri_flatten(round(res[,2],digits = 4),collapse = ""),stri_flatten(res[,2],collapse = ""),posteriors[i,2],expr)
-    if(!((ress[1] %in% values(rhash))))
-      rhash[[ress[1]]]<-ress
-    else
-    {
-      if(ress[1] %in% keys(rhash))
-      {
-        rhash[[ress[1]]][3]<- (as.numeric(rhash[[ress[1]]][3]) + as.numeric(ress[3]))
-        if(stri_length(rhash[[ress[1]]][4])>stri_length(expr))
-          rhash[[ress[1]]][4]<-expr
-      }
-    }
-
-  }
-  res<-as.data.frame(t(values(rhash)[c(3,4),]))
-  res$V1<-as.numeric(as.character(res$V1))
-  res<-res[which(res$V1>thf),]
-  res<-res[order(res$V1, decreasing = T),]
-  clear(rhash)
-  rm(rhash)
-  res[which(res[,1]>1),1]<-1
-  colnames(res)<-c("posterior","tree")
-  return(res)
-}
 
 cosi<-function(x)cos(x/180*pi)
 sini<-function(x)sin(x/180*pi)
@@ -89,34 +28,13 @@ InvX<-function(x)
 }
 troot<-function(x)abs(x)^(1/3)
 
-MM = 10
-M = 32
+MM = 100
+M = 1
 NM= 1000
-compmax = 41
+compmax = 15
 th<-(10)^(-5)
 thf<-0.05
 
-paral<-function(X,FUN)
-{
-  return(mclapply(X = X,FUN = FUN,mc.preschedule = F, mc.cores = 32))
-}
-
-runpar<-function(vect)
-{
-
-  set.seed(as.integer(vect[23]))
-  do.call(runemjmcmc, vect[1:22])
-  vals<-values(hashStat)
-  fparam<-mySearch$fparam
-  cterm<-max(vals[1,],na.rm = T)
-  ppp<-mySearch$post_proceed_results_hash(hashStat = hashStat)
-  post.populi<-sum(exp(values(hashStat)[1,][1:NM]-cterm),na.rm = T)
-  clear(hashStat)
-  rm(hashStat)
-  rm(vals)
-  gc()
-  return(list(post.populi = post.populi, p.post =  ppp$p.post, cterm = cterm, fparam = fparam))
-}
 
 
 for(j in 1:1)
@@ -138,33 +56,38 @@ for(j in 1:1)
     formula1 = as.formula(paste(fobservs,"~ 1 +",paste0(fparams,collapse = "+")))
     # outgraphs=F
 
-    vect<-list(formula = formula1,outgraphs=F,data = data.example,latnames = c("f(data.example$pos,model=\"ar1\")","f(data.example$pos1,model=\"rw1\")","f(data.example$pos2,model=\"iid\")","f(data.example$pos3,model=\"ou\")"),estimator = estimate.inla.poisson,estimator.args =  list(data = data.example),recalc_margin = 249, save.beta = F,interact = T,relations=c("cos","sigmoid","tanh","atan","sin","erf"),relations.prob =c(0.1,0.1,0.1,0.1,0.1,0.1),interact.param=list(allow_offsprings=3,mutation_rate = 250,last.mutation = 15000, max.tree.size = 4, Nvars.max =40,p.allow.replace=0.7,p.allow.tree=0.2,p.nor=0,p.and = 0.9),n.models = 10000,unique = T,max.cpu = 4,max.cpu.glob = 4,create.table = F,create.hash = T,pseudo.paral = T,burn.in = 50,print.freq = 1000,advanced.param = list(
+    vect<-list(formula = formula1,outgraphs=F,data = data.example,latnames = c("f(data.example$pos,model=\"ar1\")","f(data.example$pos1,model=\"rw1\")","f(data.example$pos2,model=\"iid\")","f(data.example$pos3,model=\"ou\")"),estimator = estimate.inla.poisson,estimator.args =  list(data = data.example),recalc_margin = 249, save.beta = F,interact = T,relations=c("cos","sigmoid","tanh","atan","sin","erf"),relations.prob =c(0.1,0.1,0.1,0.1,0.1,0.1),interact.param=list(allow_offsprings=3,mutation_rate = 250,last.mutation = 15000, max.tree.size = 4, Nvars.max =(compmax-1),p.allow.replace=0.7,p.allow.tree=0.2,p.nor=0,p.and = 0.9),n.models = 10000,unique = T,max.cpu = 4,max.cpu.glob = 4,create.table = F,create.hash = T,pseudo.paral = T,burn.in = 50,print.freq = 1000,advanced.param = list(
       max.N.glob=as.integer(10),
       min.N.glob=as.integer(5),
       max.N=as.integer(3),
       min.N=as.integer(1),
       printable = F))
 
+    length(vect)
+    
     params <- list(vect)[rep(1,M)]
 
+    
     for(i in 1:M)
     {
+  
       params[[i]]$cpu<-i*j
-      params[[i]]$simul<-"scenario_JM_"
+      params[[i]]$simul<-"scenario_epi_"
       params[[i]]$simid<-j
+      params[[i]]$NM<-1000
+      params[[i]]$simlen<-22
     }
     gc()
+    
+    gc()
     print(paste0("begin simulation ",j))
-    results<-parall.gmj(X = params,FUN = runpar,mc.preschedule = F, mc.cores = M)
-    print(results)
-
-    wait()
+    results<-parall.gmj(X = params, M = M)
 
     resa<-array(data = 0,dim = c(compmax,M*3))
     post.popul <- array(0,M)
     max.popul <- array(0,M)
     nulls<-NULL
-
+    
     not.null<-1
     for(k in 1:M)
     {
@@ -182,10 +105,10 @@ for(j in 1:1)
       {
         not.null <- k
       }
-
+      
     }
-
-
+    
+    
     for(k in 1:M)
     {
       if(k %in% nulls)
@@ -201,16 +124,20 @@ for(j in 1:1)
         resa[,k*3]<-rep(post.popul[k],length(results[[k]]$p.post)+1)
       }else
       {
+        #idsx<-order(results[[k]]$p.post,decreasing = T,na.last = T)
         resa[,k*3-2]<-rep(results[[k]]$fparam[1],length(resa[,k*3-2]))
         resa[,k*3-1]<-rep(0,length(resa[,k*3-1]))
         resa[,k*3]<-rep(-10^9,length(resa[,k*3]))
+        max.popul[k]<- -10^9
+        post.popul[k]<- -10^9
       }
-
+      
     }
-
-
+    
+    
     gc()
     rm(results)
+    
     ml.max<-max(max.popul)
     post.popul<-post.popul*exp(-ml.max+max.popul)
     p.gen.post<-post.popul/sum(post.popul)
@@ -231,14 +158,14 @@ for(j in 1:1)
             else
               hfinal[[resa[jj,ii*3-2]]]<-hfinal[[resa[jj,ii*3-2]]]+as.numeric(resa[jj,ii*3])
           }
-
+          
         }
       }
     }
-
+    
     posteriors<-values(hfinal)
-
-    print(posteriors)
+    
+    #print(posteriors)
     clear(hfinal)
     rm(hfinal)
     rm(resa)
@@ -247,13 +174,14 @@ for(j in 1:1)
     posteriors<-as.data.frame(posteriors)
     posteriors<-data.frame(X=row.names(posteriors),x=posteriors$posteriors)
     posteriors$X<-as.character(posteriors$X)
+    
     tryCatch({
-      res1<-simplifyposteriors(X = X4,posteriors = posteriors, th,thf)
+      res1<-simplifyposteriors(X = data.example,posteriors = posteriors, th,thf,resp = "methylated_bases")
       row.names(res1)<-1:dim(res1)[1]
-      write.csv(x =res1,row.names = F,file = paste0("postGMJSIM_",j,".csv"))
+      write.csv(x =res1,row.names = F,file = paste0("postEPIGEN_",j,".csv"))
     },error = function(err){
       print("error")
-      write.csv(x =posteriors,row.names = F,file = paste0("postGMJSIM_",j,".csv"))
+      write.csv(x =posteriors,row.names = F,file = paste0("postEPIGENERR_",j,".csv"))
     },finally = {
 
       print(paste0("end simulation ",j))
