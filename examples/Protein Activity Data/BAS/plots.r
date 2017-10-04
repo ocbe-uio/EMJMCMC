87ba5d39f6c95f9beffb470e803235cf51e207dd
aliaksah <aliaksah@math.uio.no>
2017-10-04 13:36:53 +0200

    ess is added for protein activity data example

--- examples/Protein Activity Data/BAS/plots.r
+++ examples/Protein Activity Data/BAS/plots.r
@@ -234,36 +234,67 @@ write(pp6.mc, file = paste(" pp6.mc.csv"),
       append = FALSE, sep = " ")
 
 dim(mat)
+
+
+
+top100000.ess<-array(0,dim = c(100000,10))
+masses.ess<-array(0,10)
+
+for(i in 1:10)
+{
+  models.ess<- read.csv(paste0("Z:/ESS/Prot",i,"/Output/prot_Example_3000000_sweeps_output_models_history.txt"),header = T,sep = "\t",na.strings = " ")[,2]
+  models.ess<- unique(models.ess)
+  models.ess<-as.numeric(as.character(models.ess))+174.6478 
+  models.ess<-sort(models.ess,decreasing = T)
+  top100000.ess[,i]<-models.ess[1:100000]
+  masses.ess[i]<-sum(exp(models.ess))
+}
+
+
+mat <- cbind(top100000.mjmcmc,NA,top100000.bas.ep,NA,top100000.bas.unif,NA,top100000.rsthin.full,NA,top100000.bas.rst.rm,NA,top100000.bas.rst.mc,NA,top100000.ess)
+
+models.ess<- read.csv(paste0("Z:/ESS/Prot",10,"/Output/prot_Example_3000000_sweeps_output_models_history.txt"),header = T,sep = "\t",na.strings = " ")[,2]
+ms<-NULL
+mus<-NULL
+for(i in 1:1000)
+{
+  ms<-c(ms,i*3000)
+  mus<-c(mus,length(unique(models.ess[1:ms[i]])))
+}  
+
+plot(x = ms,y=ms)
+points(x=ms,y=mus)
+
 #postscript("proteintop100000.hybrid.bash.basep.ps",width=13,height=5,horizontal=TRUE)
-boxplot(mat[,1:65],xaxt="n",ylab="log(Marginal Likelihood)",xlab="Replicates",ylim=c(27,42),horizontal=FALSE,pch=".",cex.lab=1.7,cex.axis=1.5,
+boxplot(mat[,1:75],xaxt="n",ylab="log(Marginal Likelihood)",xlab="Replicates",ylim=c(27,42),horizontal=FALSE,pch=".",cex.lab=1.7,cex.axis=1.5,
         omd=c(0,0.7,0,0.7))
-mtext(side=3,at=seq(6,6*11,by=11),line=0.5,las=1,text=c("MJMCMC ","BAS\neplogp","BAS\nuniform","RST","BAS\nRST-RM","BAS\nRST-MC"),cex=1.3)
-axis(side=1,at=(1:65)[-11*(1:4)],labels=FALSE,tick=TRUE,cex=0.2)
-at.vec <- c(seq(1,10,by=2),seq(12,21,by=2),seq(23,32,by=2),seq(34,43,by=2),seq(45,54,by=2),seq(56,65,by=2))
+mtext(side=3,at=seq(7,7*11,by=11),line=0.5,las=1,text=c("MJMCMC ","BAS\neplogp","BAS\nuniform","RST","BAS\nRST-RM","BAS\nRST-MC","ESS"),cex=1.3)
+axis(side=1,at=(1:75)[-11*(1:4)],labels=FALSE,tick=TRUE,cex=0.2)
+at.vec <- c(seq(1,10,by=2),seq(12,21,by=2),seq(23,32,by=2),seq(34,43,by=2),seq(45,54,by=2),seq(56,65,by=2),seq(66,75,by=2))
 mtext(side=1,line=0.65,at=at.vec,text=rep(c(1,3,5,7,9),5),las=1,cex=1.3)
 clip(x1 = 0, x2 = 100, y1 = 26, y2 = 43) 
-abline(v=11*(1:5))
+abline(v=11*(1:6))
 
-masses<-cbind(masses.mine,masses)
+masses<-cbind(masses.mine,masses,masses.ess)
 
 #postscript("proteintop100000.hybrid.bash.basep.ps",width=13,height=5,horizontal=TRUE)
 boxplot(masses,xaxt="n",ylab="sum(exp(Marginal Likelihoods))",horizontal=FALSE,pch=".",cex.lab=1.7,cex.axis=1.5,
         omd=c(0,0.7,0,0.7))
-mtext(side=1,at=seq(1,6,by=1),line=2,las=0,text=c("MJMCMC ","BAS\neplogp","BAS\nuniform","RST","BAS\nRST-RM","BAS\nRST-MC"),cex=1.3)
+mtext(side=1,at=seq(1,7,by=1),line=2,las=0,text=c("MJMCMC ","BAS\neplogp","BAS\nuniform","RST","BAS\nRST-RM","BAS\nRST-MC","ESS"),cex=1.3)
 
-axis(side=1,at=(1:65)[-11*(1:4)],labels=FALSE,tick=TRUE,cex=0.2)
-at.vec <- c(seq(1,10,by=2),seq(12,21,by=2),seq(23,32,by=2),seq(34,43,by=2),seq(45,54,by=2),seq(56,65,by=2))
-mtext(side=1,line=0.65,at=at.vec,text=rep(c(1,3,5,7,9),5),las=1,cex=1.3)
+axis(side=1,at=(1:75)[-11*(1:5)],labels=FALSE,tick=TRUE,cex=0.2)
+at.vec <- c(seq(1,10,by=2),seq(12,21,by=2),seq(23,32,by=2),seq(34,43,by=2),seq(45,54,by=2),seq(56,65,by=2),seq(66,75,by=2))
+mtext(side=1,line=0.65,at=at.vec,text=rep(c(1,3,5,7,9),6),las=1,cex=1.3)
 clip(x1 = 0, x2 = 100, y1 = 27, y2 = 42) 
-abline(v=11*(1:5))
+abline(v=11*(1:6))
 
 setwd("/mn/anatu/ansatte-u3/aliaksah/Desktop/Untitled Folder/")
 pp6.rs<-t(as.matrix(read.csv(paste(" pp6.rs",".csv",sep = "",collapse = ""))))
 pp6.mc<-t(as.matrix(read.csv(paste(" pp6.mc",".csv",sep = "",collapse = ""))))
 barplot(pp6.mc,density = 46,border="black",main = "Marginal Inclusion (MC)",xlab = "Covariates",ylab="Probability")
 barplot(pp6.rs,density = 46,border="black",main = "Marginal Inclusion (RM)",xlab = "Covariates",ylab="Probability")
-barplot(x2,) 
-barplot()
+#barplot(x2,) 
+#barplot()
 #dev.off()
 # End: Figure 5
 barplot(pp6.mc,density = 71,border="grey")
