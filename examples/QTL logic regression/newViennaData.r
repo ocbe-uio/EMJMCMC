5bdf5fdbace5285f1ae81e6d8b88081c579aaced
Aliaksandr <aliaksah@tjilpa.uio.no>
2018-02-16 18:36:48 +0100

    Clean repository 16.02.2018

--- examples/QTL logic regression/newViennaData.r
+++
@@ -1,7 +0,0 @@
-data = read.csv("C:/Users/Aliaksandr/Downloads/Genotypingdata-EUmlauflogicregression.csv",colClasses = rep('factor', 19))
-
-dat = as.data.frame(model.matrix( ~ 0+., data[,-1]))[,-2]
-names(dat)
-names(data)
-
-write.csv(x = dat,file = "C:/Users/Aliaksandr/Downloads/qtlalzg.csv")
