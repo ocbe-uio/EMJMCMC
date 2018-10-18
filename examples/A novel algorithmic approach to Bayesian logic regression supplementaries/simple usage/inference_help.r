890106f2d6bf22b08f0cef0abbf825aa6175bec9
Aliaksandr Hubin <ahu@hpc01.ad.nr.no>
2018-10-18 11:58:22 +0200

    help examples for logic regression

--- examples/A novel algorithmic approach to Bayesian logic regression supplementaries/simple usage/inference_help.r
+++ examples/A novel algorithmic approach to Bayesian logic regression supplementaries/simple usage/inference_help.r
@@ -1,26 +1,26 @@
 #read the most recent stable version of the package
-source("https://raw.githubusercontent.com/aliaksah/EMJMCMC2016/master/R/the_mode_jumping_package2.r")
+source("https://raw.githubusercontent.com/aliaksah/EMJMCMC2016/master/R/the_mode_jumping_package4.r")
 
-#make sure that you are using Mac Os or Linux (mclapply is currently not supported for Windows)
+#make sure that you are using Mac Os or Linux (mclapply is currently not supported for Windows unless some mclapply hack function for Windows is preloaded in your R session)
 
-#simulate some Gaussian responses
+
+#simulate Gaussian responses
 
 set.seed(040590)
-X1= as.data.frame(array(data = rbinom(n = 50*1000,size = 1,prob = 0.3),dim = c(1000,50)))
-Y1=rnorm(1000,-0.7+1*(X1$V1)*(X1$V4) + 1*(X1$V8*X1$V11)+1*(X1$V5*X1$V9),1)
-X1$Y1 = Y1
+X1= as.data.frame(array(data = rbinom(n = 50*1000,size = 1,prob = runif(n = 50*1000,0,1)),dim = c(1000,50)))
+Y1=rnorm(n = 1000,mean = 1+0.7*(X1$V1*X1$V4) + 0.8896846*(X1$V8*X1$V11)+1.434573*(X1$V5*X1$V9),sd = 1)
+X1$Y1=Y1
 
-
+#specify the initial formula
 formula1 = as.formula(paste(colnames(X1)[51],"~ 1 +",paste0(colnames(X1)[-c(51)],collapse = "+")))
 data.example = as.data.frame(X1)
 
 #run the inference with robust g prior
-res = LogicRegr(formula = formula1,data = data.example,family = "Gaussian",prior = "G",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
-
-
+res4G = LogicRegr(formula = formula1,data = data.example,family = "Gaussian",prior = "G",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
+print(res4G$feat.stat)
 #run the inference with Jeffrey's prior
-res = LogicRegr(formula = formula1,data = data.example,family = "Gaussian",prior = "J",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
-
+res4J = LogicRegr(formula = formula1,data = data.example,family = "Gaussian",prior = "J",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
+print(res4J$feat.stat)
 
 
 #change to Bernoulli responses
