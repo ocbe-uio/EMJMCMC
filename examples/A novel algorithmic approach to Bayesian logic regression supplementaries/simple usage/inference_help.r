1c3d1569e861aa488ac58407db28e93515fb8af8
Aliaksandr Hubin <ahu@hpc01.ad.nr.no>
2018-10-19 09:27:45 +0200

    help example of simple usage of LogicRegr updated with the examples from the simulation studies

--- examples/A novel algorithmic approach to Bayesian logic regression supplementaries/simple usage/inference_help.r
+++ examples/A novel algorithmic approach to Bayesian logic regression supplementaries/simple usage/inference_help.r
@@ -24,13 +24,19 @@ print(res4J$feat.stat)
 
 
 #change to Bernoulli responses
+X1= as.data.frame(array(data = rbinom(n = 50*1000,size = 1,prob = 0.3),dim = c(1000,50)))
+Y1=-0.7+1*((1-X1$V1)*(X1$V4)) + 1*(X1$V8*X1$V11)+1*(X1$V5*X1$V9)
 X1$Y1=round(1.0/(1.0+exp(-Y1)))
+
+#specify the initial formula
+formula1 = as.formula(paste(colnames(X1)[51],"~ 1 +",paste0(colnames(X1)[-c(51)],collapse = "+")))
+data.example = as.data.frame(X1)
 
 
 #run the inference with robust g prior
-res = LogicRegr(formula = formula1,data = data.example,family = "Bernoulli",prior = "G",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
-
+res1G = LogicRegr(formula = formula1,data = data.example,family = "Bernoulli",prior = "G",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.2,p.surv = 0.2,ncores = 32)
+print(res1G$feat.stat)
 
 #run the inference with Jeffrey's prior
-res = LogicRegr(formula = formula1,data = data.example,family = "Bernoulli",prior = "J",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.01,p.surv = 0.2,ncores = 32)
-
+res1J = LogicRegr(formula = formula1,data = data.example,family = "Bernoulli",prior = "J",report.level = 0.5,d = 15,cmax = 2,kmax = 15,p.and = 0.9,p.not = 0.2,p.surv = 0.2,ncores = 32)
+print(res1J$feat.stat)
