6de4cc25b8d48201d08dde9bb7eafa2734d9e80d
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-11-25 13:01:09 +0100

    Roxygenized m (#1)

--- R/m.R
+++ R/m.R
@@ -1,1 +1,9 @@
-m<-function(a,b)a*b
+#' @title Product function used in the deep regression context
+#' @description Product function used in the deep regression context
+#' @param a the first argument
+#' @param b the second argument
+#' @return \code{m(a,b)}, product of the arguments a*b
+#' @examples m(10,2)
+#' @export
+#' @keywords methods models
+m <- function(a, b) a * b
