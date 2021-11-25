f7f135ca4cf7ef940f96f0e319e2bc3441675116
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-11-25 13:39:28 +0100

    Moved sigmoid() to its own file

--- R/misc.R
+++ R/misc.R
@@ -4,7 +4,3 @@
 mcgmjpar = function(X,FUN,mc.cores) parallel::mclapply(X= X,FUN = FUN,mc.preschedule = T,mc.cores = mc.cores)
 
 mcgmjpse = function(X,FUN,mc.cores) lapply(X,FUN)
-
-sigmoid<- function(x) {
- return(1/(1+(exp(-x))))
-}
---
+++ R/sigmoid.R
@@ -0,0 +1,3 @@
+sigmoid<- function(x) {
+ return(1/(1+(exp(-x))))
+}
