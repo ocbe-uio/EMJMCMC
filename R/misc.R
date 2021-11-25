1193cf45316435c87dd1c286eebb55738d90260c
Waldir Leoncio <w.l.netto@medisin.uio.no>
2021-11-25 12:51:35 +0100

    Properly importing from bigmemory
    Solves one check note.

--- R/misc.R
+++ R/misc.R
@@ -13,6 +13,7 @@ sigmoid<- function(x) {
  return(1/(1+(exp(-x))))
 }
 
+#' @importFrom bigmemory attach.resource
 parallelize<-function(X,FUN)
 {
 max.cpu <- length(X)
@@ -21,7 +22,7 @@ parallel::clusterEvalQ(cl = cl,expr = c(library(INLA),library(bigmemory)))
 if(exists("statistics"))
 {
   parallel::clusterExport(cl=cl, "statistics")
-  parallel::clusterEvalQ(cl,{statistics <- bigmemory::attach.resource(statistics);1})
+  parallel::clusterEvalQ(cl,{statistics <- attach.resource(statistics);1})
 }
 res.par <- parallel::parLapply(cl = cl, X, FUN)
 parallel::stopCluster(cl)
