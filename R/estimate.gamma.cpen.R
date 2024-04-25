109e8e53bf8976125e20455aef921cfc24590ebf
aliaksah <math.uio.no>
2024-04-25 23:31:31 +0200

    added documentations for BGNLMs estimating functions

--- R/estimate.gamma.cpen.R
+++ R/estimate.gamma.cpen.R
@@ -4,8 +4,14 @@
 #' @param data dataset
 #' @param r prior inclusion penalty parameter
 #' @param logn logn
-#' @param relat relations
-#' @return A list of `mlik`, `waic`, `dic`, and `summary.fixed`
+#' @param relat a set of nonlinear transformations in the class of BGNLMs of interest
+#' @return  A list of
+#' \describe{
+#'  \item{mlik}{marginal likelihood of the model}
+#'  \item{waic}{AIC model selection criterion}
+#'  \item{dic}{BIC model selection criterion}
+#'  \item{summary.fixed$mean}{a vector of posterior modes of the parameters}
+#' }
 #' @export
 estimate.gamma.cpen <- function(formula, data, r = 1.0 / 1000.0, logn = log(1000.0), relat = c("cos", "sigmoid", "tanh", "atan", "sin", "erf")) {
   fparam <- NULL
@@ -41,9 +47,20 @@ estimate.gamma.cpen <- function(formula, data, r = 1.0 / 1000.0, logn = log(1000
   return(list(mlik = mlik, waic = waic, dic = dic, summary.fixed = summary.fixed))
 }
 
-#' @title Estimate gamma cpen 2
-#' @inheritParams estimate.gamma.cpen
-#' @return A list of `mlik`, `waic`, `dic`, and `summary.fixed`
+#' @title Estimate marginal log posterior of a single BGNLM model with alternative defaults
+#' @importFrom stringi stri_replace_all stri_split_fixed stri_count_fixed
+#' @param formula formula
+#' @param data dataset
+#' @param r prior inclusion penalty parameter
+#' @param logn logn
+#' @param relat a set of nonlinear transformations in the class of BGNLMs of interest
+#' @return  A list of
+#' \describe{
+#'  \item{mlik}{marginal likelihood of the model}
+#'  \item{waic}{AIC model selection criterion}
+#'  \item{dic}{BIC model selection criterion}
+#'  \item{summary.fixed$mean}{a vector of posterior modes of the parameters}
+#' }
 #' @export
 estimate.gamma.cpen_2 = function(formula, data,r = 1.0/223.0,logn=log(223.0),relat=c("to23","expi","logi","to35","sini","troot","sigmoid"))
 {
@@ -94,5 +111,4 @@ sini <- function(x) sin(x / 180 * pi)
 to23 <- function(x) abs(x)^(2.3)
 to25 <- function(x) abs(x)^(2.5)
 to35 <- function(x) abs(x)^(3.5)
-troot <- function(x) abs(x)^(1 / 3)
 troot <- function(x) abs(x)^(1 / 3)
