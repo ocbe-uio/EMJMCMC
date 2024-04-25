77cdb13c0469d4f0e3696fedba2540a7d27824a3
aliaksah <math.uio.no>
2024-04-25 10:50:07 +0200

    Typo on Zellner g prior corrected in the documentation

--- R/estimate.bas.lm.R
+++ R/estimate.bas.lm.R
@@ -1,7 +1,7 @@
 #' @title Obtaining Bayesian estimators of interest from a LM model
 #' @param formula a formula object for the model to be addressed
 #' @param data a data frame object containing variables and observations corresponding to the formula used
-#' @param prior integers 1, 2 or 3 are allowed corresponding to AIC, BIC or Gelman's g-prior
+#' @param prior integers 1, 2 or 3 are allowed corresponding to AIC, BIC or Zellner's g-prior
 #' @param n sample size
 #' @param g g
 #' @return a list of
