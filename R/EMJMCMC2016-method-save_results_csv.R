df18ac37f4e3af72f46127fc9ac02e5012a04140
Waldir Leoncio <w.l.netto@medisin.uio.no>
2024-04-29 14:36:51 +0200

    Fixed `save_results_csv()` (#23)

--- R/EMJMCMC2016-method-save_results_csv.R
+++ R/EMJMCMC2016-method-save_results_csv.R
@@ -1,6 +1,6 @@
 EMJMCMC2016$methods(
   # save big.data results, if the latter are available
-  save_results_csv = function(statistics1, filename) {
-    write.bigmemory::big.matrix(x = statistics1, filename = filename)
+  save_results_csv = function(statistics1, filename = tmpfile()) {
+    bigmemory::write.big.matrix(x = statistics1, filename = filename)
   }
 )
